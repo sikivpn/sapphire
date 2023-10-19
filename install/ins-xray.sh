3d49a00081f1eaa3baf3a574e8fbfcbfd7a8ebeb
sikivpn <147810890+sikivpn@users.noreply.github.com>
2023-10-20 03:57:06 +0700

    Add files via upload

--- install/ins-xray.sh
+++ install/ins-xray.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # // wget https://github.com/${GitUser}/
-GitUser="NevermoreSSH"
+GitUser="sikivpn"
 
 # // MY IPVPS
 export MYIP=$(curl -sS ipv4.icanhazip.com)
@@ -66,7 +66,7 @@ systemctl stop nginx
 
 # // INSTALL CERTIFICATES
 mkdir /root/.acme.sh
-curl https://raw.githubusercontent.com/NevermoreSSH/sapphire/main/acme.sh -o /root/.acme.sh/acme.sh
+curl https://raw.githubusercontent.com/sikivpn/sapphire/main/acme.sh -o /root/.acme.sh/acme.sh
 chmod +x /root/.acme.sh/acme.sh
 /root/.acme.sh/acme.sh --upgrade --auto-upgrade
 /root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
